2ebde991d346ba817933d0944fdfe020054d1bcd
Mikhail Pyatykh <tryingstuff@yandex.ru>
2022-07-28 23:38:15 +0900

    feat: models

---
+++ models/cards.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    required: true,
+  },
+  likes: {
+    required: true,
+  },
+  createdAt: {
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("card", cardSchema);
