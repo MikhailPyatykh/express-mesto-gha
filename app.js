0f0ea71d14ef1deb927fa0cf65b7ad00f114286a
Mikhail Pyatykh <tryingstuff@yandex.ru>
2022-08-04 00:16:21 +0900

    feat: autotest

--- app.js
+++ app.js
@@ -23,3 +23,6 @@ app.use((req, res, next) => {
 
 app.use("/users", require("./routes/users"));
 app.use("/cards", require("./routes/cards"));
+app.all("*", (req, res) => {
+  res.status(404).send({ message: "Переданы некорректные данные URL" });
+});
